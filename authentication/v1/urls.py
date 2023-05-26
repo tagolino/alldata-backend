8f114cf0245ca1ba9d53bc652be114cdc11d20ad
Ronald Tagolino <tags0516@gmail.com>
2023-05-26 21:11:38 +0800

    Add register endpoint.

--- authentication/v1/urls.py
+++ authentication/v1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import LoginAPI
+from .views import LoginAPI, RegisterAPI
 
 urlpatterns = [
     path('login', LoginAPI.as_view(), name='login'),
+    path('register', RegisterAPI.as_view(), name='register'),
 ]
